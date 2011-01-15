777699f91f3a5c800068c74dc556f40c7f389f27
Lukas Thiemeier <lukast@cpan.org>
2011-01-15 06:26:16 +0000

    Import of LUKAST/Catalyst-TraitFor-Model-DBIC-Schema-ResultRoles-0.01 from CPAN.
    gitpan-cpan-distribution: Catalyst-TraitFor-Model-DBIC-Schema-ResultRoles
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         LUKAST/Catalyst-TraitFor-Model-DBIC-Schema-ResultRoles-0.01.tar.gz
    gitpan-cpan-author:       LUKAST
    gitpan-cpan-maturity:     released

---
+++ t/lib/MyApp.pm
@@ -0,0 +1,24 @@
+package MyApp;
+use Moose;
+use namespace::autoclean;
+
+use Catalyst::Runtime 5.80;
+
+extends 'Catalyst';
+
+our $VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+__PACKAGE__->config(
+	name => 'MyApp',
+	disable_component_resolution_regex_fallback => 1,
+
+	'Model::DB' => {
+		traits => 'ResultRoles',
+		rr_debug => 1,
+	},
+);
+
+__PACKAGE__->setup();
+
+1;
